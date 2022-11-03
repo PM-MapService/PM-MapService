e9cb65fe551ab907a0b5344e9c4ef135177f5ae8
dldmldlsy <dldmldlsy@naver.com>
2022-11-03 15:47:54 +0900

    주차구역, 위험구간, 대체경로, 노드, 건물 entity 생성 및 data, builder 어노테이션설정

--- src/main/java/com/capstone/team5/pmmap/entity/AlternativeEntity.java
+++ src/main/java/com/capstone/team5/pmmap/entity/AlternativeEntity.java
@@ -1,4 +1,14 @@
 package com.capstone.team5.pmmap.entity;
 
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
 public class AlternativeEntity {
+    private int alternativeId;
+    private String name;
+    private List<Integer> nodes;
 }
--- src/main/java/com/capstone/team5/pmmap/entity/NodeEntity.java
+++ src/main/java/com/capstone/team5/pmmap/entity/NodeEntity.java
@@ -1,4 +1,13 @@
 package com.capstone.team5.pmmap.entity;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class NodeEntity {
+    private int nodeId;
+    private String name;
+    private double latitude;
+    private double longitude;
 }
