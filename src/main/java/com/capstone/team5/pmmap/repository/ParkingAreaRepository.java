f3288c28023707b18d1fab3c89a0c59707709cfd
dldmldlsy <dldmldlsy@naver.com>
2022-11-04 01:50:23 +0900

    주차구역 레포지토리 및 mapper 작성(crud, 전체조회, 건물별조회)+yml의 mybatis 들여쓰기 수정

--- src/main/java/com/capstone/team5/pmmap/repository/ParkingAreaRepository.java
+++ src/main/java/com/capstone/team5/pmmap/repository/ParkingAreaRepository.java
@@ -1,4 +1,15 @@
 package com.capstone.team5.pmmap.repository;
 
+import com.capstone.team5.pmmap.entity.ParkingAreaEntity;
+
+import java.util.List;
+
 public interface ParkingAreaRepository {
+    int insert(ParkingAreaEntity parkingAreaEntity);
+    ParkingAreaEntity select(int parkingAreaId);
+    int update(ParkingAreaEntity parkingAreaEntity);
+    int delete(int parkingAreaId);
+
+    List<ParkingAreaEntity> selectAll();
+    List<ParkingAreaEntity> selectByBuilding(int buildingId);
 }
