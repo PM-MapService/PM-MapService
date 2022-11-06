780e6554fe8ab46840091a96a722964794a77ad6
dldmldlsy <dldmldlsy@naver.com>
2022-11-06 09:05:09 +0900

    postgresql 드라이버 설치 및 환경세팅

--- src/main/java/com/capstone/team5/pmmap/repository/ParkingAreaRepository.java
+++ src/main/java/com/capstone/team5/pmmap/repository/ParkingAreaRepository.java
@@ -1,9 +1,10 @@
 package com.capstone.team5.pmmap.repository;
 
 import com.capstone.team5.pmmap.entity.ParkingAreaEntity;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
-
+@Mapper
 public interface ParkingAreaRepository {
     int insert(ParkingAreaEntity parkingAreaEntity);
     ParkingAreaEntity select(int parkingAreaId);
