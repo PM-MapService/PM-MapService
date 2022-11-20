5fb29d04ce6495a11b58f88b386032c382a87506
prkim99@gmail.com <prkim99@gmail.com>
2022-11-21 05:22:21 +0900

    건물이름으로 건물 위치 검색

---
+++ src/main/java/com/capstone/team5/pmmap/service/BuildingService.java
@@ -0,0 +1,24 @@
+package com.capstone.team5.pmmap.service;
+
+import com.capstone.team5.pmmap.dto.NodeCoordinate;
+import com.capstone.team5.pmmap.entity.BuildingEntity;
+import com.capstone.team5.pmmap.entity.ParkingAreaEntity;
+import com.capstone.team5.pmmap.repository.BuildingRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BuildingService {
+    private final BuildingRepository buildingRepository;
+
+    public NodeCoordinate getCoordinateByName(String buildingName){
+        BuildingEntity buildingEntity = buildingRepository.selectByName(buildingName);
+        NodeCoordinate node = NodeCoordinate.builder()
+                .longitude(buildingEntity.getLongitude())
+                .latitude(buildingEntity.getLatitude())
+                .build();
+        return node;
+    }
+
+}
