903f928d5eab09b5ab0b36023ef2a8ffe696d5da
dldmldlsy <dldmldlsy@naver.com>
2022-11-27 04:26:25 +0900

    DanserSectionEntity insert, select 관련 repository, mapper 작성

--- src/main/java/com/capstone/team5/pmmap/repository/DangerSectionRepository.java
+++ src/main/java/com/capstone/team5/pmmap/repository/DangerSectionRepository.java
@@ -1,4 +1,12 @@
 package com.capstone.team5.pmmap.repository;
 
+import com.capstone.team5.pmmap.entity.DangerSectionEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface DangerSectionRepository {
+
+    int insert(DangerSectionEntity dangerSectionEntity);
+    DangerSectionEntity select(int dangerSectionId);
+    DangerSectionEntity selectByName(String dangerSectionName);
 }
