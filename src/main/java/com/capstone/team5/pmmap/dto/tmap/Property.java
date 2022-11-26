f2387d8f027637930c782c6d6caf34391d9ba325
dldmldlsy <dldmldlsy@naver.com>
2022-11-26 18:33:04 +0900

    대체경로 구현 중

---
+++ src/main/java/com/capstone/team5/pmmap/dto/tmap/Property.java
@@ -0,0 +1,22 @@
+package com.capstone.team5.pmmap.dto.tmap;
+
+public class Property {
+
+    private String description;
+    private String turnType;
+
+}
+/*
+    private String totalDistance;
+    private String totalTime;
+    private String index;
+    private String name;
+    private String direction;
+    private String nearPoiName;
+    private String nearPoiX;
+    private String nearPoiY;
+    private String intersectionName;
+    private String facilityType;
+    private String facilityName;
+    private String pointType;
+    */
