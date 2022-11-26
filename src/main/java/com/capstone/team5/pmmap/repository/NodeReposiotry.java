f20ff9710cd388d24f3ac9b7ef7a56eff8a7f195
dldmldlsy <dldmldlsy@naver.com>
2022-11-27 04:17:36 +0900

    nodeEndity insert, select 관련 repository, mapper 작성

--- src/main/java/com/capstone/team5/pmmap/repository/NodeReposiotry.java
+++ src/main/java/com/capstone/team5/pmmap/repository/NodeReposiotry.java
@@ -1,4 +1,11 @@
 package com.capstone.team5.pmmap.repository;
 
+import com.capstone.team5.pmmap.entity.NodeEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface NodeReposiotry {
+    int insert(NodeEntity nodeEntity);
+    NodeEntity select(int nodeId);
+    NodeEntity selectByName(String nodeName);
 }
